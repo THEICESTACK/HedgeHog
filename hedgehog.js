b5538d7e5da67be269e396808e475109a5a148c4
ayang10 <ayang014@gmail.com>
2018-01-13 02:41:57 -0500

    first commit

---
+++ hedgehog.js
@@ -0,0 +1,25 @@
+// HedgeHog.js  Hoping to create a modal with more accessible needs
+
+    var HhogModal = {
+
+        alert: function() {
+            function message(msg) {
+                    console.log('h', msg);
+                return msg;
+            }
+            //alert('something');
+        },
+        dialog: function() {
+
+        },
+        prompt: function() {
+
+        },
+        confirm: function() {
+
+        }
+
+
+
+    };
+
